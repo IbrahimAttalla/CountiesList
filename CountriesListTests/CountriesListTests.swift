7d975b6873822b155b491574d8c3c6625c0cdf49
Ibrahim Saber <ibrahim.saber@arabiansystems.net>
2026-01-16 18:39:23 +0200

    Initial Commit

---
+++ CountriesListTests/CountriesListTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesListTests.swift
+//  CountriesListTests
+//
+//  Created by Ibrahim Saber on 16/01/2026.
+//
+
+import Testing
+@testable import CountriesList
+
+struct CountriesListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
