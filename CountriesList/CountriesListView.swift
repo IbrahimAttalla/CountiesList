c49ded0ee703e3856b4258078728fd8225e5f38d
Ibrahim Saber <ibrahim.saber@arabiansystems.net>
2026-01-16 19:11:15 +0200

    Initial commit - remove mainStoryBoard - adding empty swiftUI view with text - up and run project for first time

---
+++ CountriesList/CountriesListView.swift
@@ -0,0 +1,18 @@
+//
+//  CountriesListView.swift
+//  CountriesList
+//
+//  Created by Ibrahim Saber on 16/01/2026.
+//
+
+import SwiftUI
+
+struct CountriesListView: View {
+    var body: some View {
+        Text("Countries List View")
+    }
+}
+
+#Preview {
+    CountriesListView()
+}
